7c74284fcb0b0644aed41033865fcb1aaab6fc62
Sainozhenko <dmytrosainozhenko@gmail.com>
2023-08-30 20:08:26 +0200

    LogOut Success Test

---
+++ src/test/java/com/ArtCake/stepDefinitions/RALogOutTests.java
@@ -0,0 +1,23 @@
+package com.ArtCake.stepDefinitions;
+
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+
+public class RALogOutTests {
+
+    @Test
+    private void logout() {
+    given()
+                .accept(ContentType.JSON)
+                .when()
+                .post("/logout")
+                .then()
+                .extract().response()
+                .prettyPrint();
+		}
+
+    }
+
