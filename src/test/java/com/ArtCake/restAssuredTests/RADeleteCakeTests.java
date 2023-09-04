a2a7d45f529df4e2e93c3e4d38d8baad84f031fc
Steitz Irene <I.Steitz@gmx.de>
2023-09-04 21:17:13 +0200

    "FIX" API tests for Swagger Cake Delete

--- src/test/java/com/ArtCake/restAssuredTests/RADeleteCakeTests.java
+++ src/test/java/com/ArtCake/restAssuredTests/RADeleteCakeTests.java
@@ -1,66 +1,35 @@
-//package com.ArtCake.restAssuredTests;
-//
-//import com.ArtCake.dto.CakeDto;
-//import io.restassured.http.ContentType;
-//import org.testng.annotations.BeforeMethod;
-//import org.testng.annotations.Test;
-//
-//import static io.restassured.RestAssured.given;
-//import static org.hamcrest.Matchers.containsString;
-//import static org.hamcrest.Matchers.equalTo;
-//
-//public class RADeleteCakeTests extends TestBase{
-//    String id;
-//
-//    @BeforeMethod
-//    public void preCondition(){
-//        CakeDto contactDto = CakeDto.builder().name("Lara")
-//                .lastName("Carlson")
-//                .email("carlson42323@gmail.com")
-//                .phone("0123456789")
-//                .address("Berlin 123")
-//                .description("My friend")
-//                .build();
-//
-//        String message = given().contentType(ContentType.JSON)
-//                .header("Authorization", TOKEN)
-//                .body(contactDto)
-//                .when()
-//                .post("contacts")
-//                .then()
-//                .extract().path("message");
-//
-//        String[] all = message.split(": ");
-//        id = all[1];
-//    }
-//
-//    @Test
-//    public void deleteContactByIdSuccessTest(){
-//        given().header("Authorization", TOKEN)
-//                .when().delete("contacts/" + id)
-//                .then().assertThat().statusCode(200)
-//                .assertThat().body("message",equalTo("Contact was deleted!"));
-//    }
-//
-//
-//@Test
-//    public void deleteContactByIdWithWorngTockenTest401(){
-//    given().header("Authorization", "e2dsdwsdwe2323")
-//            .when()
-//            .delete("contacts/" + id)
-//            .then()
-//            .assertThat()
-//            .statusCode(401)
-//            .assertThat().body("message",containsString("JWT strings must contain exactly 2"));
-//}
-//
-//@Test
-//    public void deleteContactByIdWithWrongIdTest400(){
-//     given().header("Authorization", TOKEN)
-//            .when()
-//            .delete("contacts/3232323")
-//            .then()
-//            .assertThat().statusCode(400)
-//             .assertThat().body("message",containsString("not found in your contacts!"));
-//}
-//}
+package com.ArtCake.restAssuredTests;
+
+        import io.restassured.http.Cookie;
+        import org.testng.annotations.Test;
+        import static com.ArtCake.restAssuredTests.TestBase.loginWithUser;
+        import static io.restassured.RestAssured.given;
+
+public class RADeleteCakeTests {
+    @Test
+    public void deleteCakeByIDSuccessfulTest() {
+        int cakeID= 2;
+        Cookie sessionCookie = loginWithUser("manager@mail.com", "Manager007!");
+        given()
+                .cookie(sessionCookie)
+                .queryParam("cakes", cakeID)
+                .when()
+                .delete("/api/cakes/" + cakeID)
+                .then()
+                .assertThat().statusCode(200);
+
+
+    }
+
+    @Test
+    public void deleteCakeByIDTestError404() {
+        int cakeID = 100;
+        given()
+                .queryParam("cakes", cakeID)
+                .when()
+                .delete("/api/cakes/" + cakeID)
+                .then()
+                .assertThat().statusCode(404);
+
+    }
+}
