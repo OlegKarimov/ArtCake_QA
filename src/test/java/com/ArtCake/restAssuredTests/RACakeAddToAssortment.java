a0f185e8173ec48451886de4c86c2c9118297537
Sainozhenko <dmytrosainozhenko@gmail.com>
2023-09-05 16:09:43 +0200

    UPDATE Test

--- src/test/java/com/ArtCake/restAssuredTests/RACakeAddToAssortment.java
+++ src/test/java/com/ArtCake/restAssuredTests/RACakeAddToAssortment.java
@@ -4,34 +4,50 @@ package com.ArtCake.restAssuredTests;
 import io.restassured.http.ContentType;
 import io.restassured.http.Cookie;
 import org.testng.annotations.Test;
+
+import java.util.Date;
+
 import static io.restassured.RestAssured.given;
 
 public class RACakeAddToAssortment extends TestBase{
     private Cookie sessionCookie = loginWithUser("client@gmail.com", "Client007!");
-   private Cookie sessionCookieManager = loginWithUser("manager@mail.com", "Manager007!");
+    private Cookie sessionCookieManager = loginWithUser("manager@mail.com", "Manager007!");
 
-   private String requestBody = "{\n" +
-            "  \"name\": \"blueberry-testing\",\n" +
-            "  \"ingredients\": \"milk,egg,salt...\",\n" +
-            "  \"price\": 70.5,\n" +
-            "  \"category\": \"CUPCAKES\",\n" +
-            "  \"state\": \"DELETED\"\n" +
-            "}";
-   @Test
-    public void getCakeByCategoryTest200() {
-       given()
-               .contentType(ContentType.JSON)
-               .cookie(sessionCookieManager)
-               .accept(ContentType.JSON)
-               .body(requestBody)
-               .when()
-               .post("/api/cakes")
-               .then()
-               .assertThat().statusCode(201);
+    @Test
+    public void createCakeWithUniqueName() {
+        String productName = "blueberry-cake" + new Date().getTime(); // Генерируем уникальное имя с помощью временной метки
+
+        String requestBody = "{\n" +
+                "  \"name\": \"" + productName + "\",\n" +
+                "  \"ingredients\": \"milk,egg,salt...\",\n" +
+                "  \"price\": 70.5,\n" +
+                "  \"category\": \"CUPCAKES\",\n" +
+                "  \"state\": \"DELETED\"\n" +
+                "}";
+
+        given()
+                .contentType(ContentType.JSON)
+                .cookie(sessionCookieManager)
+                .accept(ContentType.JSON)
+                .body(requestBody)
+                .when()
+                .post("/api/cakes")
+                .then()
+                .assertThat().statusCode(201);
     }
 
     @Test
-    public void forbidenCreateCake() {
+    public void forbiddenCreateCake() {
+        String productName = "blueberry-cakeFail" + new Date().getTime(); // Генерируем уникальное имя с помощью временной метки
+
+        String requestBody = "{\n" +
+                "  \"name\": \"" + productName + "\",\n" +
+                "  \"ingredients\": \"milk,egg,salt...\",\n" +
+                "  \"price\": 70.5,\n" +
+                "  \"category\": \"CUPCAKES\",\n" +
+                "  \"state\": \"DELETED\"\n" +
+                "}";
+
         given()
                 .contentType(ContentType.JSON)
                 .cookie(sessionCookie)
@@ -43,4 +59,3 @@ public class RACakeAddToAssortment extends TestBase{
                 .assertThat().statusCode(403);
     }
 }
-
